ee55ee00cfbc77f853a317372743d7ad503dfd9e
C <c.chmela@gmail.com>
2017-02-27 22:40:30 +0100

    move for the beginning into one component

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-Vue.component('newentry', {
-
+Vue.component('entry-list', {
     template:
-    `<div>
+    `
+        <div>
+            <p v-for="entry in entries">{{entry.date}} - {{entry.vibe}} - {{entry.text}}</p>
+
+            <div>
         <h2>New entry</h2>
         <select id="selectVibe" v-model="newEntry.vibe">
             <option v-for="vibe in vibes"> {{ vibe.label }} </option>
@@ -9,18 +12,24 @@ Vue.component('newentry', {
         <input type="text" placeholder="optional text" v-model="newEntry.text" />
         <button @click="addEntry">Add new entry</button>
         </div>
-        `,
+        </div>
+    `,
 
     data() {
         return {
-            newEntry: { vibe: '', date: '', text: '' },
+            entries: [
+                { vibe: 'Test', date: new Date(), text: 'Servus' }
+            ],
+            newEntry: {
+                vibe: '', date: '', text: ''
+            },
             vibes: [
                 { label: 'Froehlich' },
                 { label: 'Schrecklich' }
             ]
         }
     },
-
+    
     methods: {
         addEntry() {
             this.newEntry.date = new Date();
@@ -35,6 +44,7 @@ Vue.component('newentry', {
         }
 
     }
+
 });
 
 new Vue({
