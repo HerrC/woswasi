6e8baaf9b9278deba3c4dc7177e68e283c68769e
C <c.chmela@gmail.com>
2017-02-27 23:10:10 +0100

    add new entry to list

--- js/main.js
+++ js/main.js
@@ -2,26 +2,24 @@ Vue.component('entry-list', {
     template:
     `
         <div>
-            <p v-for="entry in entries">{{entry.date}} - {{entry.vibe}} - {{entry.text}}</p>
+            <h2>New entry</h2>
+            <select id="selectVibe" v-model="newEntry.vibe">
+                <option v-for="vibe in vibes"> {{ vibe.label }} </option>
+            </select>
+            <input type="text" placeholder="optional text" v-model="newEntry.text" />
+            <button @click="addEntry">Add new entry</button>
 
-            <div>
-        <h2>New entry</h2>
-        <select id="selectVibe" v-model="newEntry.vibe">
-            <option v-for="vibe in vibes"> {{ vibe.label }} </option>
-        </select>
-        <input type="text" placeholder="optional text" v-model="newEntry.text" />
-        <button @click="addEntry">Add new entry</button>
-        </div>
+            <p v-for="entry in entries">{{getPrettyDate(entry.date)}} - {{entry.vibe}} - {{entry.text}}</p>
+
         </div>
     `,
 
     data() {
         return {
             entries: [
-                { vibe: 'Test', date: new Date(), text: 'Servus' }
             ],
             newEntry: {
-                vibe: '', date: '', text: ''
+                vibe: '', date: null, text: ''
             },
             vibes: [
                 { label: 'Froehlich' },
@@ -32,19 +30,24 @@ Vue.component('entry-list', {
     
     methods: {
         addEntry() {
-            this.newEntry.date = new Date();
-            console.log(this.newEntry.vibe + ' ' + this.newEntry.date + ' ' + this.newEntry.text);
+            this.entries.push({
+                vibe: this.newEntry.vibe, 
+                date: new Date(),
+                text: this.newEntry.text});
+
             this.clearNewEntry();
 
         },
         clearNewEntry() {
             this.newEntry.vibe = '';
-            this.newEntry.date = '';
+            this.newEntry.date = null;
             this.newEntry.text = '';
+        }, 
+        getPrettyDate(d) {
+            return ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+                d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
         }
-
     }
-
 });
 
 new Vue({
