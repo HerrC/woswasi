da4d20fcf9916a9441f59f2061990cbf681fc0c5
C <c.chmela@gmail.com>
2017-03-07 19:56:50 +0100

    rename: vibes -> moods, vibe -> mood

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ Vue.component('new-entry', {
     template: `<div>
         <h1>New entry</h1>
         
-<img v-for="vibe in vibes" :src="vibe.path" v-bind:class="{selected:isSelected(vibe.label)}" @click="setSelected(vibe.label)"></img>
+<img v-for="mood in moods" :src="mood.path" v-bind:class="{selected:isSelected(mood.label)}" @click="setSelected(mood.label)"></img>
 <br /><br />
         <p class="control">
             <input class="textarea" type="textarea" placeholder="optional text" v-model="newEntry.text" />
@@ -15,9 +15,9 @@ Vue.component('new-entry', {
     data() {
         return {
             newEntry: {
-                vibe: '', date: null, text: ''
+                mood: '', date: null, text: ''
             },
-            vibes: [
+            moods: [
                 { label: 'Happy', path: 'icons/mood/happy.png' },
                 { label: 'Sad', path: 'icons/mood/sad.png' },
                 { label: 'Neutral', path: 'icons/mood/neutral.png' }
@@ -30,7 +30,7 @@ Vue.component('new-entry', {
         addEntry() {
             if (this.moodIsSelected()) {
                 // is set before "sending"
-                this.newEntry.vibe = this.selectedItem;
+                this.newEntry.mood = this.selectedItem;
                 this.newEntry.date = new Date();
                 Event.$emit('applied', this.newEntry);
                 this.clearNewEntry();
@@ -38,7 +38,7 @@ Vue.component('new-entry', {
             }
         },
         clearNewEntry() {
-            this.newEntry.vibe = '';
+            this.newEntry.mood = '';
             this.newEntry.date = null;
             this.newEntry.text = '';
         },
@@ -70,13 +70,18 @@ Vue.component('entry-list', {
     template:
     `
         <div>
-            <entry v-for="entry in entries">{{getPrettyDate(entry.date)}} - {{entry.vibe}} - {{entry.text}}</entry>
+            <entry v-for="entry in entries">{{getPrettyDate(entry.date)}} - {{entry.mood}} - {{entry.text}}</entry>
         </div>
     `,
 
     data() {
         return {
             entries: [
+            ],
+            moods: [
+                { label: 'Happy', path: 'icons/mood/happy.png' },
+                { label: 'Sad', path: 'icons/mood/sad.png' },
+                { label: 'Neutral', path: 'icons/mood/neutral.png' }
             ],
         }
     },
@@ -91,7 +96,7 @@ Vue.component('entry-list', {
     created() {
         Event.$on('applied', (entry) => {
             // why do i have to write here again all properties?
-            this.entries.push({ vibe: entry.vibe, date: entry.date, text: entry.text });
+            this.entries.push({ mood: entry.mood, date: entry.date, text: entry.text });
         });
     }
 });
